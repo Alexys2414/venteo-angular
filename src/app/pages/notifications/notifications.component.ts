f62f0860197f907f08575d569537f01033c38750
Natalia <nrisuenosantiago@gmai.com>
2024-02-05 09:59:50 +0100

    notification css hecho

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { app } from '../../../../server';
+import { NotificationComponent } from '../../components/notification/notification.component';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [NotificationComponent],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
