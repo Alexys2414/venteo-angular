3ea6603770b713c5f56e8dd5562417d9c8134c2e
julio cuevas <julio.cue.amo@gmail.com>
2024-02-08 10:24:00 +0100

    updated support

--- src/app/pages/support/support.component.ts
+++ src/app/pages/support/support.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { IssuesService } from '../../services/issues.service';
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [],
+  imports: [SupportComponent],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css'
 })
 export class SupportComponent {
-  
+
+  constructor(
+    private issueService: IssuesService) { }
+
 }
