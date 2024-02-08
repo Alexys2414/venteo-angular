88d8f47165272512010dca256afe33290bbc8da9
Juan Daniel Forner Garriga <jdanielforga@gmail.com>
2024-02-08 09:24:10 +0100

    creado servicio de usuarios

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url = 'http://localhost:8080/api/v1';
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.url}/users`);
+  }
+
+  create(user: User): Observable<User> {
+    return this.http.post<User>(`${this.url}/users`, user);
+  }
+
+  findById(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.url}/user/${userId}`);
+  }
+
+  update(user: User, userId: number): Observable<User>{
+    return this.http.put<User>(`${this.url}/user/${userId}`, user);
+  }
+
+  patch(user: User, userId: number): Observable<User>{
+    return this.http.patch<User>(`${this.url}/user/${userId}`, user);
+  }
+
+  delete(userId: number): Observable<User>{
+    return this.http.delete<User>(`${this.url}/user/${userId}`);
+  }
+}
