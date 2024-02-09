ec68d3151f4e7551746b232fbae6f1849338bd8a
Natalia <nrisuenosantiago@gmai.com>
2024-02-09 13:01:26 +0100

    profile added

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../../types';
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  @Input() user:User = {
+  id: 18,
+  firstName: "Natalia",
+  lastName: "Risueño",
+  userName: "vvnat",
+  email: "nrisuenosantiago@gmail.com",
+  password: "123abc",
+  repeatPassword: "123abc",
+  bornDate: new Date("2004-05-27"),
+  imageUrl:  "https://avatars.githubusercontent.com/u/118915775?v=4",
+  createdAt: new Date("2021-05-27"),
+  roleId: 1,
+  money: 1000
+  };
 }
